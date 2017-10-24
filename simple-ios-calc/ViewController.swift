3ef1a94edae23f034e0288273e43faaf54cacb8c
yongjp94 <yongjun.park.94@gmail.com>
2017-10-24 14:38:10 -0700

    Initial Commit

---
+++ simple-ios-calc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  simple-ios-calc
+//
+//  Created by Yongjun Park on 10/24/17.
+//  Copyright © 2017 Yongjun Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
